8cbbeabb2bc8058e1626bf9e602d2a1eaf398e1f
Alfredo Ferreira <15140256+aaoferreira@users.noreply.github.com>
2023-09-15 15:07:48 +0200

    CVF-95: optimise bitmask

--- src/libraries/BitFlags.sol
+++ src/libraries/BitFlags.sol
@@ -5,6 +5,6 @@ error InvalidUInt8(uint256 n);
 
 function isBitSet(bytes1 flags, uint256 bit) pure returns (bool) {
     if (bit > 7) revert InvalidUInt8(bit);
-    bytes1 mask = bytes1(uint8(1 << bit));
+    bytes1 mask = bytes1(0x01) << bit;
     return (flags & mask) != bytes1(0);
 }
