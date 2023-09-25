81ee8cfc1290e9140f3cfc641642f58857ca2c97
Alfredo Ferreira <15140256+aaoferreira@users.noreply.github.com>
2023-09-25 18:17:57 +0200

    event correct keeper reward

--- test/utils.t.sol
+++ test/utils.t.sol
@@ -1,6 +1,8 @@
 // SPDX-License-Identifier: Unlicense
 pragma solidity 0.8.20;
 
+import "forge-std/Test.sol";
+
 function bytes32ToString(bytes32 _bytes32) pure returns (string memory) {
     bytes memory bytesArray = new bytes(32);
 
@@ -9,4 +11,11 @@ function bytes32ToString(bytes32 _bytes32) pure returns (string memory) {
     }
 
     return string(bytesArray);
+}
+
+function first(Vm.Log[] memory logs, bytes memory _event) pure returns (Vm.Log memory) {
+    for (uint256 i = 0; i < logs.length; i++) {
+        if (logs[i].topics[0] == keccak256(_event)) return logs[i];
+    }
+    revert(string.concat(string(_event), " not found"));
 }
