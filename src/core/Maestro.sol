d123243efdd15aaf33389b5495e58ddc4df6cca9
Alfredo Ferreira <15140256+aaoferreira@users.noreply.github.com>
2023-09-26 19:19:30 +0200

    new linked order permutations for Maestro

--- src/core/Maestro.sol
+++ src/core/Maestro.sol
@@ -209,7 +209,20 @@ contract Maestro is IMaestro, UUPSUpgradeable, Multicall {
 
     function placeLinkedOrder(PositionId positionId, LinkedOrderParams memory params) public override returns (OrderId orderId) {
         positionNFT.validateModifyPositionPermissions(positionId);
+        orderId = _placeLinkedOrder(positionId, params);
+    }
 
+    function placeLinkedOrders(
+        PositionId positionId,
+        LinkedOrderParams memory linkedOrderParams1,
+        LinkedOrderParams memory linkedOrderParams2
+    ) public override returns (OrderId linkedOrderId1, OrderId linkedOrderId2) {
+        positionNFT.validateModifyPositionPermissions(positionId);
+        linkedOrderId1 = _placeLinkedOrder(positionId, linkedOrderParams1);
+        linkedOrderId2 = _placeLinkedOrder(positionId, linkedOrderParams2);
+    }
+
+    function _placeLinkedOrder(PositionId positionId, LinkedOrderParams memory params) private returns (OrderId orderId) {
         return orderManager.placeOnBehalfOf(
             OrderParams({
                 positionId: positionId,
@@ -245,6 +258,36 @@ contract Maestro is IMaestro, UUPSUpgradeable, Multicall {
     function cancel(OrderId orderId) public {
         if (!positionNFT.isApprovedForAll(orderManager.orders(orderId).owner, msg.sender)) revert Unauthorised(msg.sender);
         orderManager.cancel(orderId);
+    }
+
+    function cancel(OrderId orderId1, OrderId orderId2) public override {
+        cancel(orderId1);
+        cancel(orderId2);
+    }
+
+    function cancelReplaceLinkedOrder(OrderId cancelOrderId, LinkedOrderParams memory newLinkedOrderParams)
+        external
+        override
+        returns (OrderId newLinkedOrderId)
+    {
+        PositionId positionId = orderManager.orders(cancelOrderId).positionId;
+        cancel(cancelOrderId);
+        return placeLinkedOrder(positionId, newLinkedOrderParams);
+    }
+
+    function cancelReplaceLinkedOrders(
+        OrderId cancelOrderId1,
+        OrderId cancelOrderId2,
+        LinkedOrderParams memory newLinkedOrderParams1,
+        LinkedOrderParams memory newLinkedOrderParams2
+    ) external override returns (OrderId newLinkedOrderId1, OrderId newLinkedOrderId2) {
+        PositionId positionId = orderManager.orders(cancelOrderId1).positionId;
+        if (PositionId.unwrap(positionId) != PositionId.unwrap(orderManager.orders(cancelOrderId2).positionId)) {
+            revert MismatchingPositionId(cancelOrderId1, cancelOrderId2);
+        }
+
+        cancel(cancelOrderId1, cancelOrderId2);
+        (newLinkedOrderId1, newLinkedOrderId2) = placeLinkedOrders(positionId, newLinkedOrderParams1, newLinkedOrderParams2);
     }
 
     function cancelAndWithdraw(OrderId orderId, address to) public override returns (uint256) {
